7024ef0683342a4ee2fcc537738443ea625bc923
remi.odufuye <remi.odufuye@gmail.com>
2019-10-21 15:21:15 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -55,10 +55,10 @@ def naming_system
                      sub_category: {
                       label: "Genus",
                       } ,
-                                      {
+                                  
                     label: "Species",
                   sub_category: nil
-                  }
+                  
           }
 end
 
